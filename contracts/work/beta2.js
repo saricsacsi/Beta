f519542f371285b835eea7d976600e045c116e24
saricsacsi <saricsacsi@users.noreply.github.com>
2018-09-23 15:41:20 +0200

    xx

--- contracts/work/beta2.js
+++ contracts/work/beta2.js
@@ -1,132 +1,281 @@
 
 const Web3 = require('web3');
-const web3 = new Web3();
 
 
-// CALL
 /*
-_owner
-admin
-beneficiary.....
+ call the public variables:
+_owner 
+admin 
 */
-
-function getAdminrOfWallet(address, abi) {
+function getOwnerOfWallet(web3, address, abi, callback) {
     var res
-    var Contract = web3.eth.contract(abi);
-        // initiate contract for an address
-    var BetaWalletContract = Contract.at(address);
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
           
-        BetaWalletContract.admin(function(error, res) {
+       
+    try {
+        BetaWalletContract.admin(function(res,error) {
+            if (!error) {
+                callback(res, 0);   
+            }
+            else {
+                callback(null, error);
+                }
+            });
+    } catch (err) {
+        callback(0, err);
+    }
+}
 
-          });
-          
-        }
-
-function getOwnerOfWallet(address, abi) {
+function getAdminrOfWallet(web3, address, abi, callback) {
     var res
-    var Contract = web3.eth.contract(abi);
-        // initiate contract for an address
-    var BetaWalletContract = Contract.at(address);
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
           
-        BetaWalletContract._owner(function(error, res) {
+       
+    try {
+        BetaWalletContract.admin(function(res,error) {
+            if (!error) {
+                callback(res, 0);   
+            }
+            else {
+                callback(null, error);
+                }
+            });
+    } catch (err) {
+        callback(0, err);
+    }
+}
 
-          });
-          
-        }
 
 
 
 /*
-
+call the getter functions:
 getPendingTransactions()
 walletBalance()
 walletBalanceOfToken()
+check_permitting(address _who, uint _transactionId)
 */
-function getPendingTransactions(address, abi, callback) {
+function getPendingTransactions(web3, address, abi, callback) {
     var res
-    var BetaWalletContract =  new web3.eth.Contract(abi, address);
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
         // initiate contract for an address
-    //var WalletContracBetat = Contract.at(address);
+    
             try {
-                BetaWalletContract.getPendingTransactions(function(error, res) {
+                BetaWalletContract.getPendingTransactions(function(res,error) {
                     if (!error) {
-                        callback(res);   
+                        callback(res, 0);   
                     }
-                      });
+                    else {
+                        callback(null, error);
+                        }
+                    });
             } catch (err) {
-                callback(err);
+                callback(0, err);
             }
         }
 
 
-function walletBalance(address, abi) {
+function walletBalance(web3, address, abi, callback) {
     var res,balance
-    var Contract = web3.eth.contract(abi);
-        // initiate contract for an address
-    var BetaWalletContract = Contract.at(address);
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+
             try {
                 BetaWalletContract.walletBalance(function(error, res) {
                     if (!error) {
                         var balance = web3.fromWei(res, 'ether');
+                        callback(balance, 0);   
                     }
-                });
+                    else {
+                        callback(null, error);
+                        }
+                    });
             } catch (err) {
-                
+                callback(0, err);
             }
         }
 
 
-function BalanceOfToken(address, abi) {
+function BalanceOfToken(web3, address, abi, callback) {
     var res,balance
-    var Contract = web3.eth.contract(abi);
-        // initiate contract for an address
-    var BetaWalletContract = Contract.at(address);
-           try {
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+
+            try {
                 BetaWalletContract.walletBalanceOfToken(function(error, res) {
-                   if (!error) {                    
-                   var balance = web3.fromWei(res, 'ether');
-                
+                    if (!error) {
+                        var balance = web3.fromWei(res, 'ether');
+                        callback(balance, 0);   
                     }
-                });
+                    else {
+                        callback(null, error);
+                        }
+                    });
             } catch (err) {
-               
+                callback(0, err);
             }
         }
 
- 
-
-
+function check_permitting(web3, address, abi, who, transactionId, callback) {
+    var res
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+          // initiate contract for an address
+    
+            try {
+                 BetaWalletContract.check_permitting(who, transactionId, function(res,error) {
+                     if (!error) {
+                         callback(res, 0);   
+                      }
+                    else {
+                          callback(null, error);
+                          }
+                     });
+            } catch (err) {
+                callback(0, err);
+            }
+          }
+        
 /*
-// SEND
-
-/*
+call the functions what are changing the state on the network:
 setNewAdmin(address _newAdmin)
 withdraw_ether()
 withdraw_token(uint _amount)
 
 transferToToken(address to, uint amount, uint32 typeofTransaction)
 signTransaction(uint transactionId)
- deleteTransaction(uint transactionId)
+deleteTransaction(uint transactionId)
  */ 
-function transferToToken(address, abi, beneficiary, amount, type) {
-    var res
-    var Contract = web3.eth.contract(abi);
-        // initiate contract for an address
-    var BetaWalletContract = Contract.at(address);
-    //web3.eth.defaultAccount = '0x0006bD3CEbF6ecD2A05018853bd4EB44187c267c';
-    
-    try {
-        BetaWalletContract.transferToToken(beneficiary, amount, type,function(error, res) {
-           if (!error) {                    
-            
-            }
-        });
-    } catch (err) {
-        
-    }
-}
 
+
+ 
+
+function transferToToken(web3, address, abi, beneficiary, amount, type, callback) {
+    var res
+       
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+
+            try {
+                BetaWalletContract.transferToToken(beneficiary, amount, type, function(error, res) {
+                    if (!error) {
+                        callback(res, 0);   
+                    }
+                    else {
+                        callback(null, error);
+                        }
+                    });
+            } catch (err) {
+                callback(0, err);
+            }
+        }
+
+
+        
+function signTransaction(web3, address, abi,transactionId, callback) {
+    var res
+                       
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+            try {
+                BetaWalletContract.signTransaction(transactionId, function(error, res) {
+                    if (!error) {
+                         callback(res, 0);   
+                    }
+                    else {
+                        callback(null, error);
+                        }
+                     });
+             } catch (err) {
+                callback(0, err);
+            }
+        }  
+                
+
+        
+function deletePendingTransaction(web3, address, abi,transactionId, callback) {
+    var res
+                                       
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+                try {
+                    BetaWalletContract.deletePendingTransaction(transactionId, function(error, res) {
+                     if (!error) {
+                            callback(res, 0);   
+                     }
+                        else {
+                            callback(null, error);
+                            }
+                     });
+            } catch (err) {
+                callback(0, err);
+               }
+         }       
+        
+         
+function setNewAdmin(web3, address, abi, newAdmin, callback) {
+    var res
+                                       
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+                try {
+                    BetaWalletContract.setNewAdmin(newAdmin, function(error, res) {
+                     if (!error) {
+                            callback(res, 0);   
+                     }
+                        else {
+                            callback(null, error);
+                            }
+                     });
+            } catch (err) {
+                callback(0, err);
+               }
+         }       
+    
+         
+function withdraw_ether(web3, address, abi, callback) {
+    var res
+                                               
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+                try {
+                    BetaWalletContract.withdraw_ether(function(error, res) {
+                        if (!error) {
+                             callback(res, 0);   
+                        }
+                         else {
+                            callback(null, error);
+                             }
+                        });
+            } catch (err) {
+                callback(0, err);
+                 }
+            }       
+      
+            
+   
+         
+function withdraw_token(web3, address, abi, token_amount, callback) {
+    var res
+                                                           
+    const BetaWalletContract =  new web3.eth.Contract(abi, address);
+                try {
+                    BetaWalletContract.withdraw_token(token_amount,function(error, res) {
+                        if (!error) {
+                             callback(res, 0);   
+                          }
+                         else {
+                             callback(null, error);
+                              }
+                        });
+            } catch (err) {
+                 callback(0, err);
+                 }
+             }       
+                           
 module.exports = {
+    getOwnerOfWallet,
+    getAdminrOfWallet,
     getPendingTransactions,
-    transferToToken
+    walletBalance,
+    BalanceOfToken,
+    check_permitting,
+    transferToToken,
+    signTransaction,
+    deletePendingTransaction,
+    setNewAdmin,
+    withdraw_ether,
+    withdraw_token
   }
--- contracts/work/main.js
+++ contracts/work/main.js
@@ -1,9 +1,77 @@
-const { getPendingTransactions,
-    transferToToken } = require('./beta2.js')
+const {
+    getOwnerOfWallet,
+    getAdminrOfWallet,
+    getPendingTransactions,
+    walletBalance,
+    BalanceOfToken,
+    check_permitting,
+    transferToToken,
+    signTransaction,
+    deletePendingTransaction,
+    setNewAdmin,
+    withdraw_ether,
+    withdraw_token
+  } = require('./beta2.js')
 
-const address = '0x1353f2A2CA0c839189c351483B29Fc6785c402cd'
-const abi = require('./abi_BW_v2.json')
+const address = '0x1353f2A2CA0c839189c351483B29Fc6785c402cd';
+const abi = require('./abi_BW_v2.json');
+const Web3 = require('web3');
+const web3 = new Web3();
 
-getPendingTransactions(address, abi, function() {
-    console.log('ok');
+const who = '0x1353f2A2CA0c839189c351483B29Fc6785c402cd';
+const beneficiary = '0x006EC6E3F6225eb6E802C65532ceD362A04D382a';
+
+const amount = 1000;
+
+const type = 0;
+const transactionId = 1;
+const token_amount = 100;
+const newAdmin = '0x004EE9C43b57fB75c4D5752d6F15A0117c04E6d2';
+
+getOwnerOfWallet(web3, address, abi, function(result , error) {
+    console.log(result);
+})
+
+getAdminrOfWallet(web3, address, abi, function(result , error) {
+    console.log(result);
+})
+
+getPendingTransactions(web3, address, abi, function(result , error) {
+    console.log(result);
+})
+
+walletBalance(web3, address, abi, function(result , error) {
+    console.log(result);
+})
+
+BalanceOfToken(web3, address, abi, function(result , error) {
+    console.log(result);
+})
+
+check_permitting(web3, address, abi, who, transactionId,  function(result , error) {
+    console.log(result);
+})
+
+transferToToken(web3, address, abi, beneficiary, amount, type,  function(result , error) {
+    console.log(result);
+})
+
+signTransaction(web3, address, abi,transactionId, function(result , error) {
+    console.log(result);
+})
+
+deletePendingTransaction(web3, address, abi,transactionId,  function(result , error) {
+    console.log(result);
+})
+
+setNewAdmin(web3, address, abi, newAdmin,  function(result , error) {
+    console.log(result);
+})
+
+withdraw_ether(web3, address, abi, function(result , error) {
+    console.log(result);
+})
+
+withdraw_token(web3, address, abi, token_amount, function(result , error) {
+    console.log(result);
 })
